41cb40d92c4cfd881710b5f494c7faadab5b7c28
Soulriv <darkstarsoulriv@gmail.com>
2018-12-26 20:23:43 +0200

    [+] Basic view

---
+++ ecommerce/src/views/ProductCard.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import {View, Text, Image, ScrollView, StyleSheet, Button, Picker, Modal} from 'react-native'
+
+class ProductCard extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render () {
+    return (
+      <View>
+        <Text>Hello</Text>
+      </View>
+    )
+  }
+}
+
+export default connect(null, null)(Home)
